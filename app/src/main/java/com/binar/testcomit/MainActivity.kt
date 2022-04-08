0460108c73d7a87f61f4106906a31d0411415570
rizqifakhri1 <rizqifakhri@gmail.com>
2022-04-08 20:06:41 +0700

    Initial commit

---
+++ app/src/main/java/com/binar/testcomit/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.binar.testcomit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //A1
+        //A2
+        //A3
+        //A4
+    }
+}
